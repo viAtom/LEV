89ad7731afc481cc05eeba3b82b8a3acd10cd56c
Tom <tomy199517@gmail.com>
2017-01-29 04:30:21 +0100

    master: changing team headers to general header

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,264 +2,265 @@ var lang = "en_US"; // "en_US"
 var output = "";
 var lev = angular.module('lev', []);
 lev.controller('lev-controller', function($scope) {
-    $scope.players = false;
-    $scope.loading = true;
-    $scope.ddragon = "http://ddragon.leagueoflegends.com/cdn/";
-    $scope.items = {};
-    $scope.game = false;
-    $scope.games = false;
-    $scope.general = false;
-    $scope.redTeam = {};
-    $scope.blueTeam = {};
-    $scope.annonces = [];
-    $scope.loadingPerc = 0;
-    $scope.loadingMaxPerc = 20;
-    $scope.loadingMessage = "Please wait while loading .. ";
-    $scope.isReplaying = false;
-    $scope.Math = window.Math;
-    var intPerc = setInterval(function() {
-        if ($scope.loading) {
-            if ($scope.loadingPerc < $scope.loadingMaxPerc) {
-                $scope.loadingPerc++;
-                $scope.$apply();
-            }
-        }
-    }, 10);
+	$scope.players = false;
+	$scope.loading = true;
+	$scope.ddragon = "http://ddragon.leagueoflegends.com/cdn/";
+	$scope.items = {};
+	$scope.game = false;
+	$scope.games = false;
+	$scope.general = false;
+	$scope.blueTeam = {};
+	$scope.redTeam = {};
+	$scope.annonces = [];
+	$scope.loadingPerc = 0;
+	$scope.loadingMaxPerc = 20;
+	$scope.loadingMessage = "Please wait while loading .. ";
+	$scope.isReplaying = false;
+	$scope.Math = window.Math;
+	var intPerc = setInterval(function() {
+		if ($scope.loading) {
+			if ($scope.loadingPerc < $scope.loadingMaxPerc) {
+				$scope.loadingPerc++;
+				$scope.$apply();
+			}
+		}
+	}, 10);
 
-    $.getJSON("https://ddragon.leagueoflegends.com/api/versions.json", function(data) {
-        $scope.ddragon += data[0];
-        $scope.loadingMaxPerc += 20;
-        $scope.$apply();
-        $.getJSON($scope.ddragon + "/data/" + lang + "/item.json", function(data) {
-            $scope.items = data;
-            $scope.loadingMaxPerc += 20;
-            $scope.$apply();
-        });
-    })
+	$.getJSON("https://ddragon.leagueoflegends.com/api/versions.json", function(data) {
+		$scope.ddragon += data[0];
+		$scope.loadingMaxPerc += 20;
+		$scope.$apply();
+		$.getJSON($scope.ddragon + "/data/" + lang + "/item.json", function(data) {
+			$scope.items = data;
+			$scope.loadingMaxPerc += 20;
+			$scope.$apply();
+		});
+	})
 
-    $.getJSON('http://api.lolesports.com/api/issueToken', function(issueData) {
-        $scope.loadingMaxPerc += 20;
-        $scope.socket = new WebSocket('ws://livestats.proxy.lolesports.com/stats?jwt=' + issueData.token);
-        $scope.socket.onopen = function() {
-            $scope.loadingMaxPerc += 20;
-        };
-        $scope.socket.onerror = function() {
-            $scope.loadingMaxPerc = 0;
-            $scope.loadingMessage = "Couldn't connect to Riot LiveStats";
-            $scope.$apply();
-        }
-        $scope.socket.onmessage = function(evt) {
-            var data = JSON.parse(evt.data);
-            $scope.general = data;
-            $scope.games = Object.keys(data);
-            $scope.game = Object.keys(data).reverse()[0];
-            if ($scope.loading) {
-                $scope.players = data[$scope.game].playerStats;
-                $scope.loading = false;
-                clearInterval(intPerc)
-                $scope.getTeamPlayers();
-                $scope.socket.onmessage = function(evt) {
-                    var data = JSON.parse(evt.data);
-                    $scope.execData(data);
-                }
-            } else alert('Error not loading');
-            $scope.$apply();
-        };
-    });
+	$.getJSON('http://api.lolesports.com/api/issueToken', function(issueData) {
+		$scope.loadingMaxPerc += 20;
+		$scope.socket = new WebSocket('ws://livestats.proxy.lolesports.com/stats?jwt=' + issueData.token);
+		$scope.socket.onopen = function() {
+			$scope.loadingMaxPerc += 20;
+		};
+		$scope.socket.onerror = function() {
+			$scope.loadingMaxPerc = 0;
+			$scope.loadingMessage = "Couldn't connect to Riot LiveStats";
+			$scope.$apply();
+		}
+		$scope.socket.onmessage = function(evt) {
+			var data = JSON.parse(evt.data);
+			$scope.general = data;
+			$scope.games = Object.keys(data);
+			$scope.game = Object.keys(data).reverse()[0];
+			if ($scope.loading) {
+				$scope.players = data[$scope.game].playerStats;
+				$scope.loading = false;
+				clearInterval(intPerc)
+				$scope.getTeamPlayers();
+				$scope.socket.onmessage = function(evt) {
+					var data = JSON.parse(evt.data);
+					$scope.execData(data);
+				}
+			} else alert('Error not loading');
+			$scope.$apply();
+		};
+	});
 
-    $scope.execData = function(data) {
-        $.each(data, function(gameUpdated, value) {
-            if ($scope.general[gameUpdated] == undefined) {
-                $scope.general[gameUpdated] = value;
-                $scope.games.push(gameUpdated);
-                $scope.chooseGame(gameUpdated);
-            } else {
-                $.each(data[gameUpdated].playerStats, function(i, val) {
-                    for (key in val) {
-                        $scope.general[gameUpdated].playerStats[i][key] = val[key];
-                    }
-                });
-                $.each(data[gameUpdated].teamStats, function(i, val) {
-                    for (key in val) $scope.general[gameUpdated].teamStats[i][key] = val[key];
-                })
-                $scope.general[gameUpdated].t = data[gameUpdated].t;
-                $scope.general[gameUpdated].gameComplete = data[gameUpdated].gameComplete;
-            }
-        });
-        try { $scope.$apply(); } catch (e) { console.log(e); }
-    }
+	$scope.execData = function(data) {
+		$.each(data, function(gameUpdated, value) {
+			if ($scope.general[gameUpdated] == undefined) {
+				$scope.general[gameUpdated] = value;
+				$scope.games.push(gameUpdated);
+				$scope.chooseGame(gameUpdated);
+			} else {
+				$.each(data[gameUpdated].playerStats, function(i, val) {
+					for (key in val) {
+						$scope.general[gameUpdated].playerStats[i][key] = val[key];
+					}
+				});
+				$.each(data[gameUpdated].teamStats, function(i, val) {
+					for (key in val) $scope.general[gameUpdated].teamStats[i][key] = val[key];
+				})
+				$scope.general[gameUpdated].t = data[gameUpdated].t;
+				$scope.general[gameUpdated].gameComplete = data[gameUpdated].gameComplete;
+			}
+		});
+		try { $scope.$apply(); } catch (e) { console.log(e); }
+	}
 
-    $scope.chooseGame = function(game) {
-        if ($scope.isReplaying) {
-            $scope.isReplaying = false;
-            var lastData = {};
-            lastData[$scope.game] = replayGames[$scope.game];
-            setTimeout(function() {
-                $scope.execData(lastData);
-            }, 250);
-        }
-        $scope.game = game;
-        $scope.getTeamPlayers();
-    }
+	$scope.chooseGame = function(game) {
+		if ($scope.isReplaying) {
+			$scope.isReplaying = false;
+			var lastData = {};
+			lastData[$scope.game] = replayGames[$scope.game];
+			setTimeout(function() {
+				$scope.execData(lastData);
+			}, 250);
+		}
+		$scope.game = game;
+		$scope.getTeamPlayers();
+	}
 
-    $scope.replay = function() {
-        $scope.isReplaying = true;
-        var script = "";
-        switch ($scope.game) {
-            case "1001960052":
-                script = "SKTSSGG1.js";
-                break;
-            case "1001960063":
-                script = "SKTSSGG2.js";
-                break;
-            case "1001960064":
-                script = "SKTSSGG3.js";
-                break;
-            case "1001960065":
-                script = "SKTSSGG4.js";
-                break;
-            case "1001960067":
-                script = "SKTSSGG5.js";
-                break;
-            default:
-                return false;
-                break;
-        }
+	$scope.replay = function() {
+		$scope.isReplaying = true;
+		var script = "";
+		switch ($scope.game) {
+			case "1001960052":
+				script = "SKTSSGG1.js";
+				break;
+			case "1001960063":
+				script = "SKTSSGG2.js";
+				break;
+			case "1001960064":
+				script = "SKTSSGG3.js";
+				break;
+			case "1001960065":
+				script = "SKTSSGG4.js";
+				break;
+			case "1001960067":
+				script = "SKTSSGG5.js";
+				break;
+			default:
+				return false;
+				break;
+		}
 
-        $.get('assets/js/' + script, function(data) {
-            eval(data);
+		$.get('assets/js/' + script, function(data) {
+			eval(data);
 
-            function execReplay(i) {
-                if (replayGame[i] && $scope.isReplaying) {
-                    $scope.execData(replayGame[i]);
-                    setTimeout(function() { execReplay(i + 1) }, 1000);
-                } else return true;
-            }
-            setTimeout(function() { execReplay(0) }, 10);
-        });
-    }
+			function execReplay(i) {
+				if (replayGame[i] && $scope.isReplaying) {
+					$scope.execData(replayGame[i]);
+					setTimeout(function() { execReplay(i + 1) }, 1000);
+				} else return true;
+			}
+			setTimeout(function() { execReplay(0) }, 10);
+		});
+	}
 
-    $scope.showLog = function() {
-        output = $scope.general[$scope.game].playerStats;
-        console.log("Active players stats logged in var 'output'");
-    };
+	$scope.showLog = function() {
+		output = $scope.getInfoTeam('blue');
+		console.log("logged in var 'output'");
+	};
 
-    $scope.getAnnonces = function() {
-        if ($scope.annonces.length == 0) return "";
-        else {
-            var annonce = $scope.annonces[0];
-        }
-    }
+	$scope.getAnnonces = function() {
+		if ($scope.annonces.length == 0) return "";
+		else {
+			var annonce = $scope.annonces[0];
+		}
+	}
 
-    $scope.getTeamPlayers = function() {
-        var iB = 1,
-            iR = 1;
-        $scope.players = $scope.general[$scope.game].playerStats;
-        $.each($scope.players, function(i, player) {
-            if (player.teamId == 100) {
-                $scope.blueTeam[iB] = $scope.players[i];
-                iB++;
-            } else {
-                $scope.redTeam[iR] = $scope.players[i];
-                iR++;
-            }
-        });
-    }
+	$scope.getTeamPlayers = function() {
+		var iB = 1,
+			iR = 1;
+		$scope.players = $scope.general[$scope.game].playerStats;
+		$.each($scope.players, function(i, player) {
+			if (player.teamId == 100) {
+				$scope.blueTeam[iB] = $scope.players[i];
+				iB++;
+			} else {
+				$scope.redTeam[iR] = $scope.players[i];
+				iR++;
+			}
+		});
+	}
 
-    getTeamNumber = function(team) {
-        if (team == "blue" || team == 100) return 100;
-        else if (team == "red" || team == 200) return 200;
-        else throw "Unknown team '" + team + "'";
-    }
+	getTeamNumber = function(team) {
+		if (team == "blue" || team == 100) return 100;
+		else if (team == "red" || team == 200) return 200;
+		else throw "Unknown team '" + team + "'";
+	}
 
-    $scope.getTeam = function(team) {
-        if (getTeamNumber(team) == 100) return $scope.blueTeam;
-        else if (getTeamNumber(team) == 200) return $scope.redTeam;
-        else throw "Unknown team '" + team + "'";
-    }
+	$scope.getTeam = function(team) {
+		if (getTeamNumber(team) == 100) return $scope.blueTeam;
+		else if (getTeamNumber(team) == 200) return $scope.redTeam;
+		else throw "Unknown team '" + team + "'";
+	}
 
-    $scope.getInfoTeam = function(team) {
-        var k = 0,
-            d = 0,
-            a = 0;
-        var gteam = $scope.getTeam(team);
-        var infoTeam = {}
-        $.each(gteam, function(i, player) {
-            k += player.kills;
-            d += player.deaths;
-            a += player.assists;
-        });
+	$scope.getInfoTeam = function(team) {
+		if ($scope.game) {
+			var k = 0,
+				d = 0,
+				a = 0;
+				tg = 0;
+			var gteam = $scope.getTeam(team);
+			var infoTeam = $scope.general[$scope.game].teamStats[getTeamNumber(team)];
+			$.each(gteam, function(i, player) {
+				k += player.kills;
+				d += player.deaths;
+				a += player.assists;
+				tg += player.tg;
+			});
 
-        infoTeam['kills'] = k;
-        infoTeam['deaths'] = d;
-        infoTeam['assists'] = a;
-        infoTeam['name'] = gteam[1].summonerName.substring(0, 3);
+			infoTeam['kills'] = k;
+			infoTeam['deaths'] = d;
+			infoTeam['assists'] = a;
+			infoTeam['tg'] = tg;
+			infoTeam['name'] = gteam[1].summonerName.split(" ")[0];
 
-        $.each($scope.general[$scope.game].teamStats[getTeamNumber(team)], function(i, val) {
-            infoTeam[i] = val;
-        });
+			return [infoTeam];
+		} else return {};
+	}
 
-        return infoTeam;
-    }
+	$scope.getNameMatch = function(match) {
+		var matchGeneral = $scope.general[match];
+		if (matchGeneral) {
+			var matchName = matchGeneral.generatedName.split("|");
+			var blueTeam = matchName[0];
+			var redTeam = matchName[1];
+			var matchNumber = matchName[2].substr(1);
+			return blueTeam + " vs " + redTeam + " Game " + matchNumber;
+		} else return "error";
+	}
 
-    $scope.getNameMatch = function(match) {
-        var matchGeneral = $scope.general[match];
-        var matchName = matchGeneral.generatedName.split("|");
-        var blueTeam = matchName[0];
-        var redTeam = matchName[1];
-        var matchNumber = matchName[2].substr(1);
-        return blueTeam + " vs " + redTeam + " Game " + matchNumber;
-    }
+	$scope.getDiffGold = function(player) {
+		return $scope.blueTeam[player].tg - $scope.redTeam[player].tg;
+	}
 
-    $scope.getDiffGold = function(player) {
-        return $scope.blueTeam[player].tg - $scope.redTeam[player].tg;
-    }
+	$scope.getDiffGoldColor = function(player) {
+		var diff = $scope.getDiffGold(player);
+		if (diff == 0) return ""
+		else if (diff > 0) return "background-color: rgb(35, 103, 118);"
+		else return "background-color: rgb(110, 43, 48);"
+	}
 
-    $scope.getDiffGoldColor = function(player) {
-        var diff = $scope.getDiffGold(player);
-        if (diff == 0) return ""
-        else if (diff > 0) return "background-color: rgb(35, 103, 118);"
-        else return "background-color: rgb(110, 43, 48);"
-    }
+	function pad(n) {
+		if (n < 10) return "0" + n;
+		else return n;
+	}
 
-    function pad(n) {
-        if (n < 10) return "0" + n;
-        else return n;
-    }
+	$scope.getTime = function() {
+		var time = $scope.general[$scope.game].t;
+		return pad(Math.round(time / 1000 / 60)) + ":" + pad(Math.round(time / 1000 % 60));
+	}
 
-    $scope.getTime = function() {
-        var time = $scope.general[$scope.game].t;
-        return pad(Math.round(time / 1000 / 60)) + ":" + pad(Math.round(time / 1000 % 60));
-    }
+	$scope.hover = function(id) {
+		$('#' + id).addClass('hovered');
+		$('#' + id + ' .champion-square').mouseover();
+	}
 
-    $scope.hover = function(id) {
-        $('#' + id).addClass('hovered');
-        $('#' + id + ' .champion-square').mouseover();
-    }
-
-    $scope.unhover = function(id) {
-        $('#' + id).removeClass('hovered');
-        $('#' + id + ' .champion-square').mouseout();
-    }
+	$scope.unhover = function(id) {
+		$('#' + id).removeClass('hovered');
+		$('#' + id + ' .champion-square').mouseout();
+	}
 });
 lev.directive('tooltip', function() {
-    return function(scope, element, attrs) {
-        attrs.$observe('title', function(title) {
-            element.tooltip('destroy');
-            if (jQuery.trim(title)) element.tooltip();
-        })
-        element.on('$destroy', function() {
-            element.tooltip('destroy');
-            delete attrs.$$observers['title'];
-        });
-    }
+	return function(scope, element, attrs) {
+		attrs.$observe('title', function(title) {
+			element.tooltip('destroy');
+			if (jQuery.trim(title)) element.tooltip();
+		})
+		element.on('$destroy', function() {
+			element.tooltip('destroy');
+			delete attrs.$$observers['title'];
+		});
+	}
 });
-lev.filter('kda', function() { return function(infoTeam) { return infoTeam.kills + " / " + infoTeam.deaths + " / " + infoTeam.assists; }; });
-lev.filter('name', function() { return function(infoTeam) { return infoTeam.name; }; });
 
 lev.config(function($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist([
-        'self',
-        'http://ddragon.leagueoflegends.com/**'
-    ]);
+	$sceDelegateProvider.resourceUrlWhitelist([
+		'self',
+		'http://ddragon.leagueoflegends.com/**'
+	]);
 });
